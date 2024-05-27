2e560304a67a3fcd247a8d088fc9a2791a63083f
manav <manavpatel13402003@gmail.com>
2024-05-27 09:17:43 +0530

    change function... create db

--- lib/src/database/database_helper.dart
+++ lib/src/database/database_helper.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
 import 'database_creator.dart';
 import 'table_creator.dart';
@@ -21,21 +19,13 @@ class DatabaseHelper {
     required String tableName,
     required Map<String, String> columns,
   }) async {
-    dbName = dbName ?? 'default.db';
-    tableName = tableName ?? 'default_table';
-    final isCreated = await isDatabaseCreated(dbName, tableName);
-    if (isCreated) {
-      final databasesPath = await getDatabasesPath();
-      final databasePath = join(databasesPath, dbName);
-      print('Database created at: $databasePath');
-
+    try {
       final database = await DatabaseCreator.createDatabase(dbName: dbName);
       await TableCreator.createTable(
         db: database,
         tableName: tableName,
         columns: columns,
       );
-
       Fluttertoast.showToast(
         msg: 'Database and table created successfully',
         toastLength: Toast.LENGTH_SHORT,
@@ -45,9 +35,9 @@ class DatabaseHelper {
         textColor: Colors.white,
         fontSize: 16.0,
       );
-    } else {
+    } catch (e) {
       Fluttertoast.showToast(
-        msg: 'Error creating database',
+        msg: 'Error creating database: $e',
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.BOTTOM,
         timeInSecForIosWeb: 1,
@@ -57,6 +47,49 @@ class DatabaseHelper {
       );
     }
   }
+
+  // static Future<void> createDatabaseAndTable({
+  //   required BuildContext context,
+  //   required String dbName,
+  //   required String tableName,
+  //   required Map<String, String> columns,
+  // }) async {
+  //   dbName = dbName;
+  //   tableName = tableName;
+  //   final isCreated = await isDatabaseCreated(dbName, tableName);
+  //   if (isCreated) {
+  //     final databasesPath = await getDatabasesPath();
+  //     final databasePath = join(databasesPath, dbName);
+  //     print('Database created at: $databasePath');
+  //
+  //     final database = await DatabaseCreator.createDatabase(dbName: dbName);
+  //     await TableCreator.createTable(
+  //       db: database,
+  //       tableName: tableName,
+  //       columns: columns,
+  //     );
+  //
+  //     Fluttertoast.showToast(
+  //       msg: 'Database and table created successfully',
+  //       toastLength: Toast.LENGTH_SHORT,
+  //       gravity: ToastGravity.BOTTOM,
+  //       timeInSecForIosWeb: 1,
+  //       backgroundColor: Colors.green,
+  //       textColor: Colors.white,
+  //       fontSize: 16.0,
+  //     );
+  //   } else {
+  //     Fluttertoast.showToast(
+  //       msg: 'Error creating database',
+  //       toastLength: Toast.LENGTH_SHORT,
+  //       gravity: ToastGravity.BOTTOM,
+  //       timeInSecForIosWeb: 1,
+  //       backgroundColor: Colors.red,
+  //       textColor: Colors.white,
+  //       fontSize: 16.0,
+  //     );
+  //   }
+  // }
 
   static Future<int> insert({
     required Map<String, dynamic> values,
